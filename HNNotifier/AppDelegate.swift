3317ccc2bad656961090b9ed16762541764e33b9
Kun <likunarmstrong@gmail.com>
2014-08-11 22:26:55 -0600

    Initial Commit

---
+++ HNNotifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HNNotifier
+//
+//  Created by Kun Li on 8/11/14.
+//
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HNNotifier/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  HNNotifier
+//
+//  Created by Kun Li on 8/11/14.
+//
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
